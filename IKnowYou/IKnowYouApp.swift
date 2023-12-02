f22ab5855f4f740b3de5565cd88aca336a03fe21
netolobo <desilio@gmail.com>
2023-12-02 15:26:37 -0800

    Initial Commit

---
+++ IKnowYou/IKnowYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  IKnowYouApp.swift
+//  IKnowYou
+//
+//  Created by Neto Lobo on 02/12/23.
+//
+
+import SwiftUI
+
+@main
+struct IKnowYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
